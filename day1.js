dc2e937088e03a71a109d6f9ccf7e560878a01d8
Smiteshbhore21 <smiteshbhore23@gmail.com>
2024-08-22 00:44:52 +0530

    Commit day1.js

---
+++ day1.js
@@ -0,0 +1,35 @@
+/*
+    DATA TYPES :-
+    1. undefined -> not defined;
+    2. null -> nothing;
+    3. boolean -> True or False;
+    4. string -> Alpha-Numberic Text;
+    5. symbol -> Those are immutable;
+    6. number -> all numbers;
+    7. object -> object can hold a lot of different key and value pairs;
+*/
+/*
+    var -> data type can be change and accesible throughout the code;
+    let -> within the scope, where that is declared
+    const -> value can not be change
+*/
+
+var sum = 3+4;
+var diff = 77-7;
+var prod = 3*7;
+var div = 8/2;
+
+console.log("Addition of "+3+" and "+4+" is "+sum);
+console.log("Diffrence of "+77+" and "+7+" is "+diff);
+console.log("Product of "+3+" and "+7+" is "+prod);
+console.log("Division of "+8+" by "+2+" is "+div);
+
+var incre = 0;
+console.log("Incre before "+incre);
+incre++;
+console.log("Incre after "+incre);
+
+var decre = 0;
+console.log("decre before "+decre);
+decre--;
+console.log("decre after "+decre);
