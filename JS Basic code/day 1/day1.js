bbe706559e8760a460f771f8a160801606ca3fcf
nishantcg13 <galandesonu42@gmail.com>
2024-08-22 00:58:42 +0530

    Day 1

---
+++ JS Basic code/day 1/day1.js
@@ -0,0 +1,59 @@
+// Hello this is Nishant
+// Author : Nishant C Galande
+// Date : 21 August 2024
+
+let n = 8 // this is inline comment
+/*suppose if i have to write a bunch of lines 
+in my code for documentation then 
+i will use multi-line comment 
+denoted using this comment typo*/
+
+/*
+JavaScript Provides 7 different datatypes
+1. Undefined - Nothing defined
+2. Null - nothing
+3. Number - numbers
+4. String - text
+5. Symbol - immutable primitive unique value
+6. Boolean -true or false
+7. Object - object can hold a lot of different type of data in it
+*/
+
+var myName = "Nishant" // like dynamic in dart and can be changed at runtime
+
+myName = 8 
+// var is used throughout the program 
+
+let name = "nishant"
+// let is used with in the scope where you declare it
+
+const pi = 3.14
+
+// const needs to be initialized at the time of declaration and cannot be changed never if we try to change it it will go error
+ 
+//**************************************************************************************************************
+// new code
+
+var a = null;
+console.log(a); // null
+
+a = 2;
+console.log(a);
+
+var a = 10;
+var b = 5;
+
+console.log("Addition of "+a+" & "+b+ " is = "+(a+b));
+console.log("substraction of "+a+" & "+b+ " is = "+(a-b));
+console.log("multiplication of "+a+" & "+b+ " is = "+(a*b));
+console.log("division of "+a+" & "+b+ " is = "+(a/b));
+
+var increment = 10;
+console.log("before increment "+increment);
+increment++;
+console.log("after increment "+increment);
+
+var decrement = 10;
+console.log(" before decrement "+decrement);
+decrement--;
+console.log(" after decrement "+decrement);
