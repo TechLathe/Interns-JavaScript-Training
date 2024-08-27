c5b500138cfe0e826ecf83bc063a16e6744fc728
nishantcg13 <galandesonu42@gmail.com>
2024-08-27 22:32:25 +0530

    Day 7

---
+++ JS Basic code/day 7/day7.js
@@ -0,0 +1,65 @@
+// This is day 7 of js
+var collection = {
+    "2548": {
+        "album": "Slippery When Wet",
+        "artist": "Bon Jovi",
+        "tracks": [
+            "Let It Rock",
+            "You Give Love a Bad Name"
+        ]
+    },
+
+    "2468": {
+        "album": " 1999",
+        "artist": "Prince",
+        "tracks": [
+            " 1999",
+            "Little Red Corvette"
+        ]
+    },
+
+    "1245": {
+        "artist": "Robert Palmer",
+        "tracks": []
+    },
+    "5439": {
+        "album": "ABBA Gold"
+    }
+
+};
+// Keep a copy of the collection for tests
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+// Only change code below this line
+function updateRecords(id, prop, value) { 
+    if(value === ""){
+        delete collection[id][prop]
+    }else if(prop == "tracks"){
+        collection[id][prop] = collection[id][prop] || [];
+        collection[id][prop].push(value)
+    }else{
+        collection[id][prop] = value
+    }
+    return collection;
+}
+updateRecords(2468,"tracks","test");
+console.log(updateRecords(5439,"artist","ABBA"));
+
+// loops
+
+var myArray = []
+
+var i =0;
+while(i<5){
+    myArray.push(i)
+    i++;
+}
+
+console.log(myArray)
+
+var our = []
+
+for(let i = 0;i<=5;i++){
+    our.push(i)
+}
+
+console.log(our)
