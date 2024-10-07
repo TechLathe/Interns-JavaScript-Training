9e36932f053a2b7f21859f68dde40a1e134b7847
Smiteshbhore21 <smiteshbhore23@gmail.com>
2024-10-07 23:33:10 +0530

    react-real-estate-site 02

--- React-Real-Estate/src/components/Hero/Hero.jsx
+++ React-Real-Estate/src/components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import CountUp from "react-countup";
 import { HiLocationMarker } from 'react-icons/hi';
+
 const Hero = () => {
     return (
         <section className="hero-wrapper">
@@ -14,18 +16,46 @@ const Hero = () => {
                         </h1>
                     </div>
                     <div className="flexColStart hero-des">
-                        <span>
-                            Find a variety of properties that suit you very easilty
+                        <span className="secondaryText">
+                            Find a variety of properties that suit you very easily
                         </span>
-                        <span>
+                        <span className="secondaryText">
                             Forget all difficulties in finding a residence for you
                         </span>
                     </div>
 
-                    <div className="search-bar">
-                        <HiLocationMarker color="var{--blue}" size={25} />
-                        <input type="text" />
+                    {/* Search bar */}
+                    <div className="flexCenter search-bar">
+                        <HiLocationMarker color="var(--blue)" size={25} />
+                        <input type="text" placeholder="Enter location" />
                         <button className="button">Search</button>
+                    </div>
+
+                    {/* Stats Section */}
+                    <div className="flexCenter stats">
+                        <div className="flexColStart stat">
+                            <span>
+                                <CountUp start={8800} end={9000} duration={4} />
+                                <span>+</span>
+                            </span>
+                            <span className="secondaryText">Premium Products</span>
+                        </div>
+
+                        <div className="flexColStart stat">
+                            <span>
+                                <CountUp start={1950} end={2000} duration={4} />
+                                <span>+</span>
+                            </span>
+                            <span className="secondaryText">Happy Customers</span>
+                        </div>
+
+                        <div className="flexColStart stat">
+                            <span>
+                                <CountUp end={28} />
+                                <span>+</span>
+                            </span>
+                            <span className="secondaryText">Award Winning</span>
+                        </div>
                     </div>
                 </div>
 
@@ -40,4 +70,4 @@ const Hero = () => {
     );
 }
 
-export default Hero
+export default Hero;
