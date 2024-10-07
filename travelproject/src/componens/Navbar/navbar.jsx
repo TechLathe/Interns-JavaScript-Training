f9ac520b73bda24161b4194e5797ddd3b7401481
purvighatage <purvighatage@gmail.com>
2024-10-07 21:53:53 +0530

    07oct

--- travelproject/src/componens/Navbar/navbar.jsx
+++ travelproject/src/componens/Navbar/navbar.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {usestate}from 'react';
 import "./navbar.css";
 import {BiLogoMediumOld} from 'react-icons/bi';
 import { PiDotsNineBold } from 'react-icons/bi';
 import { AiFillCloseCircle} from 'react-icons/bi;'
 const navbar =() => {
+    const[active,setactive]=usestate('navbar')
+    const showNav=()=>{
+        setactive('navBar activeNavbar')
+    }
     return (
+        <section className='navBarSection'>
         <div className='navbar'>
         <div className="logodiv">
             <BiLogoMediumOld classname='icon'/>
@@ -31,6 +36,7 @@ const navbar =() => {
         <button className='signUpBtn byn'>sign up</button>
         <PiDotsNineBold className='icon'/>
         </div>
+        </section>
     )
 }
 export default navbar
