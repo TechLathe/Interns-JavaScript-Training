f4a6b04a09b8424d4317f8a39e75a20d0b8b3873
purvighatage <purvighatage@gmail.com>
2024-10-08 21:53:46 +0530

    08oct

---
+++ travelproject/src/componens/app.js
@@ -0,0 +1,16 @@
+import react from 'react';
+import'./app.css';
+import about from './componens/about/about';
+import blog from './componens/blog/blog';
+import footer from'./componens/footer/footer';
+import home from './componens/home/home';
+import navbar from './componens/navbar/navbar';
+import offer from './componens/offers/offer';
+import popular from './componens/popular/popular';
+const app =() => {
+    return{
+        
+
+    }
+}
+export default app 
--- travelproject/src/componens/home/home.jsx
+++ travelproject/src/componens/home/home.jsx
@@ -3,7 +3,22 @@ import "./home.css";
 
 const home =() => {
     return (
-        <div>home section</div>
+        <section className='home'>
+            <div className='setcontainer container'>
+                <div className='hometext'>
+                    <h1 className='title'>
+                        plan your trip with travel dot
+                    </h1>
+
+                    <p className='subtitle'>
+                        travel to your favourite city with us
+                    </p>
+                    <button className='btn'>
+                        <a href="a">explore home</a>
+                    </button>
+                </div>
+            </div>
+        </section>
     )
 }
 export default home
